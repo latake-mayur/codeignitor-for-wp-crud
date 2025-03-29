66ecf5c2fc17eaf852d12389b6d2fa269fd03a98
unknown <latakemayur@gmail.com>
2025-03-29 23:56:42 +0530

    login page and css

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -5,12 +5,10 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
-class Login extends BaseController
+class PostController extends BaseController
 {
     public function index()
     {
-        // Login Page
-        return view("wp/login_view");
-        // echo '<h1>Mayura The manifester</h1>';
+        //
     }
 }
